8c352083433517369bebb26e551bd5d65528b902
unknown <mandshah@ca-to-intern10.us.int.genesyslab.com>
2015-10-08 12:11:37 -0400

    Formatting

--- sample-api/streaming.js
+++ sample-api/streaming.js
@@ -1,20 +1,22 @@
-
 function DataStream() {
   var randomGenerator = function() {
-    return Math.floor(Math.random()*100 + 1);
+    return Math.floor(Math.random() * 100 + 1);
   }
   return {
     getMeta: function() {},
-    getLastSnapshot:  function() {},
-    getDataStream:  function() {
-      var responseStream = Rx.Observable.create(function (observer) {
+    getLastSnapshot: function() {},
+    getDataStream: function() {
+      var responseStream = Rx.Observable.create(function(observer) {
         var cnt = 0;
         setInterval(function() {
           cnt++;
           var dataSnapshotSample = {
             timestamp: new Date(),
             columns: ['Toronto', 'New York', 'Kiev'],
-            data: [[randomGenerator(), randomGenerator(), randomGenerator()], [0,2,0]]
+            data: [
+              [randomGenerator(), randomGenerator(), randomGenerator()],
+              [0, 2, 0]
+            ]
           };
           observer.onNext(dataSnapshotSample);
         }, 2000);
@@ -25,13 +27,20 @@ function DataStream() {
 }
 
 var metaDataSample = {
-  columnsMap: {'Toronto':0, 'New York': 1, 'Kiev': 2}
+  columnsMap: {
+    'Toronto': 0,
+    'New York': 1,
+    'Kiev': 2
+  }
 };
 
 var dataSnapshotSample = {
   timestamp: new Date(),
   columns: ['Toronto', 'New York', 'Kiev'],
-  data: [[10, 20, 30], [0,2,0]]
+  data: [
+    [10, 20, 30],
+    [0, 2, 0]
+  ]
 };
 
 
