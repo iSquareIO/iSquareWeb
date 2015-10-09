96ae988b3764ee6cf0a872bbfd098d661825050e
Vasiliy Malakhov <mvamgc@gmail.com>
2015-10-09 00:06:27 -0400

    Result on main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,27 @@
  * Controller of the iSquareWebApp
  */
 angular.module('iSquareWebApp')
-  .controller('MainCtrl', ['dataStream', function (dataStream) {
+  .controller('MainCtrl', ['dataStream', '$rootScope', function (dataStream, $rootScope) {
 
     var stream = dataStream.getDataStream('sse');
+    var self = this;
     stream.subscribe(function(dataSnapshot) {
       console.log("dataSnapshot: ");
       console.log(dataSnapshot);
       console.log(JSON.stringify(dataSnapshot));
+      $rootScope.$apply(function() {
+        var data = JSON.parse(dataSnapshot);
+        var table = [];
+        for(var i=0; i<data.columns.length; i++) {
+          table.push({
+            city: data.columns[i],
+            value: data.data[0][i],
+            alert: data.data[1][i]
+          });
+        }
+        self.table = table;
+        self.data = data;
+      });
     });
 
     this.awesomeThings = [
